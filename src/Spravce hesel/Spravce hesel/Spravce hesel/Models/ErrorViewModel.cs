e90b2fc6d324ada80d541200896674ca5be6eaac
Jakub Kosinka <kosinka@spstrutnov.cz>
2022-09-14 13:16:03 +0200

    Revert "Pridan sulution"
    This reverts commit 30543949f2a119246c832185e798205c3ddac09c.

--- src/Spravce hesel/Spravce hesel/Spravce hesel/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Spravce_hesel.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
