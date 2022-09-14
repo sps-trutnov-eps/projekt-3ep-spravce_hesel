30543949f2a119246c832185e798205c3ddac09c
Jakub Kosinka <kosinka@spstrutnov.cz>
2022-09-14 13:16:03 +0200

    Pridan sulution

---
+++ src/Spravce hesel/Spravce hesel/Spravce hesel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Spravce_hesel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
