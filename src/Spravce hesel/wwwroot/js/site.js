89a052952f12d2e54e76d2ac9139de0df0f952bc
Jan Kleistner <kleistner@spstrutnov.cz>
2022-10-16 14:46:23 +0200

    Oprava chyby v cookies

--- src/Spravce hesel/wwwroot/js/site.js
+++ src/Spravce hesel/wwwroot/js/site.js
@@ -1,7 +1,7 @@
 ﻿function PrepnoutBarevnyMotiv() {
     let cookies = document.cookie;
 
-    if (cookies.length == 0)
+    if (!cookies.includes("TmavyMotiv"))
         document.cookie = "TmavyMotiv=true; path=/; expires=Fri, 31 Dec 9999 23:59:59 GMT";
     else {
         document.cookie = "TmavyMotiv=; path=/; max-age=0";
