dc87dd757a7ca3f349803e9acecc39413ac56d40
Jan Kleistner <kleistner@spstrutnov.cz>
2022-12-06 19:09:31 +0100

    Systém vykreslování nápovědy

---
+++ src/Spravce hesel/wwwroot/js/napoveda.js
@@ -0,0 +1,18 @@
+﻿$.getJSON("../json/napoveda.json", function (napoveda) {
+
+    const ROUTE = document.getElementById("ROUTE").innerHTML;
+    const prehravac_obsah = document.getElementById("napoveda_prehravac_obsah");
+    const prehravac_nadpis = document.getElementById("napoveda_prehravac_nadpis")
+
+    const sekce = ROUTE.split("-");
+    let aktualniNapoveda = napoveda;
+
+    sekce.forEach(rt => {
+        aktualniNapoveda = aktualniNapoveda[rt];
+    })
+
+    prehravac_nadpis.innerHTML = aktualniNapoveda["Nazev"];
+    prehravac_obsah.innerHTML = aktualniNapoveda["Obsah"];
+
+})
+
