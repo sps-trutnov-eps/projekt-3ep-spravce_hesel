da0691fbe02472ccd2638625ecc236496d1306da
Jan Kleistner <kleistner@spstrutnov.cz>
2022-12-06 20:21:53 +0100

    Úprava navigace nápovědou

--- src/Spravce hesel/wwwroot/js/napoveda.js
+++ src/Spravce hesel/wwwroot/js/napoveda.js
@@ -1,18 +1,46 @@
-﻿$.getJSON("../json/napoveda.json", function (napoveda) {
+﻿let ROUTE = document.getElementById("ROUTE").innerHTML.split("-");
 
-    const ROUTE = document.getElementById("ROUTE").innerHTML;
-    const prehravac_obsah = document.getElementById("napoveda_prehravac_obsah");
-    const prehravac_nadpis = document.getElementById("napoveda_prehravac_nadpis")
+function reload(sekce) {
+    $.getJSON("../json/napoveda.json", function (napoveda) {
 
-    const sekce = ROUTE.split("-");
-    let aktualniNapoveda = napoveda;
+        const prehravac_obsah = document.getElementById("napoveda_prehravac_obsah");
+        const prehravac_nadpis = document.getElementById("napoveda_prehravac_nadpis")
 
-    sekce.forEach(rt => {
-        aktualniNapoveda = aktualniNapoveda[rt];
+        let aktualniNapoveda = napoveda;
+
+        try {
+            sekce.forEach(rt => {
+                aktualniNapoveda = aktualniNapoveda[rt];
+            })
+
+            prehravac_nadpis.innerHTML = aktualniNapoveda["Nazev"];
+            prehravac_obsah.innerHTML = aktualniNapoveda["Obsah"];
+        }
+
+        catch {
+            prehravac_nadpis.innerHTML = "Došlo k chybě!";
+            prehravac_obsah.innerHTML = "Obsah této nápovědy není nyní přístupný!";
+        }
+
     })
+}
 
-    prehravac_nadpis.innerHTML = aktualniNapoveda["Nazev"];
-    prehravac_obsah.innerHTML = aktualniNapoveda["Obsah"];
+function zpet() {
+    if (ROUTE.length != 1) {
+        ROUTE.pop();
+        reload(ROUTE);
+    }
+}
 
-})
+function rozbalitMenu(tlacitko) {
+    if (document.getElementById("napoveda_menu_seznam").className == "skryty") {
+        document.getElementById("napoveda_menu_seznam").className = "";
+        tlacitko.innerHTML = "▲";
+    }
+    else {
+        document.getElementById("napoveda_menu_seznam").className = "skryty";
+        tlacitko.innerHTML = "▼";
+    }
+}
 
+reload(ROUTE);
