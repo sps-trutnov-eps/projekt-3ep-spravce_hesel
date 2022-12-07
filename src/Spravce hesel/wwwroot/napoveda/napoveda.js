044389902bd4a452b3416d74b4a6cb84fde607cb
Jan Kleistner <kleistner@spstrutnov.cz>
2022-12-07 19:17:13 +0100

    Oprava chyb

--- src/Spravce hesel/wwwroot/napoveda/napoveda.js
+++ src/Spravce hesel/wwwroot/napoveda/napoveda.js
@@ -1,4 +1,4 @@
-﻿function ziskatPodkategorie(napoveda, adresa = null) {
+﻿function ziskatNavigaci(napoveda, navigace = false, adresa = null) {
     let html = "";
     let podsekce = Object.keys(napoveda);
     podsekce = podsekce.reverse();
@@ -6,39 +6,20 @@
     podsekce = podsekce.reverse();
 
     if (podsekce.length != 0) {
-        if (adresa == null) {
+        if (navigace)
+            html += '<ul id="napoveda_menu_seznam" class="skryty">';
+        else
             html += "<ul>";
-            podsekce.forEach(sekce => {
-                html += "<li>" + napoveda[sekce]["Nazev"];
-                html += ziskatPodkategorie(napoveda[sekce]);
-                html += "</li>";
-            });
-            html += "</ul>";
-        } else {
-            html += "<ul>";
-            podsekce.forEach(sekce => {
+        podsekce.forEach(sekce => {
+            if (adresa == null) {
+                html += '<li><a href="/Napoveda/' + sekce + '">' + napoveda[sekce]["Nazev"];
+                html += ziskatNavigaci(napoveda[sekce], false, sekce);
+            } else {
                 let aktualniAdresa = adresa + "-" + [sekce];
                 html += '<li><a href="/Napoveda/' + aktualniAdresa + '">' + napoveda[sekce]["Nazev"];
-                html += ziskatPodkategorie(napoveda[sekce], aktualniAdresa);
-                html += "</a></li>";
-            });
-            html += "</ul>";
-        }
-    }
-
-    return html;
-}
-
-function ziskatNavigaci(napoveda) {
-    let html = "";
-    let podsekce = Object.keys(napoveda);
-
-    if (podsekce.length != 0) {
-        html += '<ul id="napoveda_menu_seznam" class="skryty">';
-        podsekce.forEach(sekce => {
-            html += '<li><a href="/Napoveda/' + sekce + '">' + napoveda[sekce]["Nazev"];
-            html += ziskatPodkategorie(napoveda[sekce], sekce);
-            html += "</li>";
+                html += ziskatNavigaci(napoveda[sekce],false, aktualniAdresa);
+            }
+            html += "</a></li>";
         });
         html += "</ul>";
     }
@@ -60,8 +41,7 @@ function load(sekce) {
             let aktualniNapoveda = napoveda;
 
             document.getElementById("napoveda_navigace_menu").innerHTML =
-                document.getElementById("napoveda_navigace_menu").innerHTML + ziskatNavigaci(napoveda);
-
+                document.getElementById("napoveda_navigace_menu").innerHTML + ziskatNavigaci(napoveda, true);
 
             try {
                 sekce.forEach(rt => {
@@ -78,7 +58,7 @@ function load(sekce) {
                     .then(text => prehravacObsah.innerHTML = text);
                 document.getElementById("napoveda_navigace_menu_cesta").innerHTML = cesta;
 
-                document.getElementById("napoveda_podsekce").innerHTML = ziskatPodkategorie(aktualniNapoveda);
+                document.getElementById("napoveda_podsekce").innerHTML = ziskatNavigaci(aktualniNapoveda, false, document.getElementById("ROUTE").innerHTML);
 
                 $("#napoveda_navigace_rozbalit").click(() => {
                     if (document.getElementById("napoveda_menu_seznam").className === "skryty") {
@@ -100,10 +80,14 @@ function load(sekce) {
 
 $("#napoveda_navigace_zpet").click(() => {
     if (ROUTE.length != 1) {
-        ROUTE.pop();
+        document.getElementById("ROUTE").innerHTML = document.getElementById("ROUTE").innerHTML.replace(ROUTE[ROUTE.length - 1], "");
+        document.getElementById("ROUTE").innerHTML =
+            document.getElementById("ROUTE")
+                .innerHTML.substring(0, document.getElementById("ROUTE").innerHTML.length - 1);
+
+        ROUTE = document.getElementById("ROUTE").innerHTML.split("-");
         load(ROUTE);
     }
 });
-
 
 load(ROUTE);
